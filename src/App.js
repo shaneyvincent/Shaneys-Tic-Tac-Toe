130d94f57e427b9347a913cd6bf4a840b5a194e4
Shane Vincent <shaneyshaneshane5@gmail.com>
2018-06-18 16:12:52 -0400

    changed array syntax, called box value

--- src/App.js
+++ src/App.js
@@ -2,17 +2,19 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  constructor(){
+  constructor(props){
+    super(props)
     this.state = {
-      board = Array (9).fill("X")
+      board : Array (9).fill("X")
     }
   }
   render() {
-     const box = this.state.board.map(box => <div className="box">{box}</div>)
+     const Box = this.state.board.map(box => <div className="box">{box}</div>)
     return (
       <div className="container">
         <h1> Shaneys Tic Tac Toe App </h1>
         <div className="board">
+        {Box}
           // <div className="box">X</div>
           // <div className="box">X</div>
           // <div className="box"></div>
