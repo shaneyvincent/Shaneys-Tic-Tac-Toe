fb729c6306c840a2a1921d0a63d2fafcdbe82696
Shane Vincent <shaneyshaneshane5@gmail.com>
2018-06-18 16:03:04 -0400

    added array to map out 9 boxes instead of creating 9 individual boxes/ commented out individual boxes

--- src/App.js
+++ src/App.js
@@ -2,20 +2,26 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    this.state = {
+      board = Array (9).fill("X")
+    }
+  }
   render() {
+     const box = this.state.board.map(box => <div className="box">{box}</div>)
     return (
       <div className="container">
         <h1> Shaneys Tic Tac Toe App </h1>
         <div className="board">
-          <div className="box">X</div>
-          <div className="box">X</div>
-          <div className="box"></div>
-          <div className="box"></div>
-          <div className="box"></div>
-          <div className="box"></div>
-          <div className="box"></div>
-          <div className="box"></div>
-          <div className="box"></div>
+          // <div className="box">X</div>
+          // <div className="box">X</div>
+          // <div className="box"></div>
+          // <div className="box"></div>
+          // <div className="box"></div>
+          // <div className="box"></div>
+          // <div className="box"></div>
+          // <div className="box"></div>
+          // <div className="box"></div>
         </div>
       </div>
     );
