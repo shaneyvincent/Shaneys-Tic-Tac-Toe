bf583f6031cc90dea4f754170b9f871bc570d832
Shane Vincent <shaneyshaneshane5@gmail.com>
2018-06-19 11:02:17 -0400

    added second player radio button and a start value

--- src/components/choosePlayer.js
+++ src/components/choosePlayer.js
@@ -11,6 +11,14 @@ class Player extends Component {
           <input type="radio" name="player" value="LOS" />
           </label>
 
+          <label>
+
+        Player NEGROS
+          <input type="radio" name="player" value="NEGROS" />
+          </label>
+
+          <input type="submit" value="Start" />
+
        </form>
       )
     }
